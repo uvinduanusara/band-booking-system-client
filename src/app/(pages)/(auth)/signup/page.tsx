67a5c4eb292a6c2226ca3121ac47e5ec9ce137ce
Uvindu Anusara <uvindu03@gmail.com>
2025-11-17 15:19:43 +0530

    [user-navigation-sign-up] *Updated

--- src/app/(pages)/(auth)/signup/page.tsx
+++ src/app/(pages)/(auth)/signup/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -8,16 +10,21 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Eye, EyeOff } from "lucide-react";
 import ArtistBookLogo from "../../../../../assets/ArtistsBookLogo.svg";
 import AuthCover from "../../../../../assets/AuthCoverImage.jpg";
 import SignupImage from "../../../../../assets/SignupImage.jpg";
 import Google from "../../../../../assets/Google.svg";
-import Facebook from "../../../../../assets/Facebook.svg"
-import Apple from "../../../../../assets/Apple.svg"
+import Facebook from "../../../../../assets/Facebook.svg";
+import Apple from "../../../../../assets/Apple.svg";
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
   return (
     <div className="relative bg-white overflow-hidden">
       <Image
@@ -34,59 +41,109 @@ export default function SignUp() {
       />
 
       <div className="flex justify-center items-center min-h-screen w-full">
-        <div className="rounded-2xl flex w-[1000px] shadow-2xl overflow-hidden mb-20 h-[650px]">
+        <div className="rounded-2xl flex w-[1000px] shadow-2xl overflow-hidden mb-20 h-[700px]">
           <div className="w-[50%]">
-            <Card className=" bg-white backdrop-blur-md shadow-2xl rounded-none h-[650px]">
+            <Card className=" bg-white backdrop-blur-md shadow-2xl rounded-none h-[700px]">
               <CardHeader>
-                <CardTitle className="text-center justify-center text-emerald-800 text-4xl font-bold font-['Montserrat'] mt-4">SIGN UP</CardTitle>
+                <CardTitle className="text-center justify-center text-emerald-800 text-4xl font-bold font-['Montserrat'] mt-4">
+                  SIGN UP
+                </CardTitle>
               </CardHeader>
               <CardContent className="ml-10 mr-10">
-                <form >
+                <form>
                   <div className="flex flex-col gap-6">
                     <div className="grid gap-2">
-                      <Label htmlFor="username">Username</Label>
+                      <Label
+                        htmlFor="username"
+                        className="justify-center text-black text-base font-normal font-poppins"
+                      >
+                        Username
+                      </Label>
                       <Input
                         id="username"
                         type="username"
                         placeholder="Enter your username"
                         required
-                        className="bg-zinc-100"
+                        className="bg-zinc-100 justify-center text-stone-500 text-xs font-normal font-poppins"
                       />
                     </div>
                     <div className="grid gap-2">
-                      <Label htmlFor="email">Email</Label>
+                      <Label
+                        htmlFor="email"
+                        className="justify-center text-black text-base font-normal font-poppins"
+                      >
+                        Email
+                      </Label>
                       <Input
                         id="email"
                         type="email"
                         placeholder="Enter your email"
                         required
-                        className="bg-zinc-100"
+                        className="bg-zinc-100 justify-center text-stone-500 text-xs font-normal font-poppins"
                       />
                     </div>
                     <div className="grid gap-2">
                       <div className="flex items-center">
-                        <Label htmlFor="password">Password</Label>
+                        <Label
+                          htmlFor="password"
+                          className="justify-center text-black text-base font-normal font-poppins"
+                        >
+                          Password
+                        </Label>
                       </div>
-                      <Input
-                        id="password"
-                        type="password"
-                        placeholder="Enter your Password"
-                        required
-                        className="bg-zinc-100"
-                      />
+                      <div className="relative">
+                        <Input
+                          id="password"
+                          type={showPassword ? "text" : "password"}
+                          placeholder="Enter your Password"
+                          required
+                          className="bg-zinc-100 justify-center text-stone-500 text-xs font-normal font-poppins pr-10"
+                        />
+                        <button
+                          type="button"
+                          className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500 hover:text-gray-700"
+                          onClick={() => setShowPassword(!showPassword)}
+                        >
+                          {showPassword ? (
+                            <EyeOff size={16} />
+                          ) : (
+                            <Eye size={16} />
+                          )}
+                        </button>
+                      </div>
                     </div>
                     <div className="grid gap-2">
-                      <Label htmlFor="confirm-password">Confirm Password</Label>
-                      <Input
-                        id="confirm-password"
-                        type="confirm-password"
-                        placeholder="Enter your password"
-                        required
-                        className="bg-zinc-100"
-                      />
+                      <Label
+                        htmlFor="confirm-password"
+                        className="justify-center text-black text-base font-normal font-poppins"
+                      >
+                        Confirm Password
+                      </Label>
+                      <div className="relative">
+                        <Input
+                          id="confirm-password"
+                          type={showConfirmPassword ? "text" : "password"}
+                          placeholder="Enter your password"
+                          required
+                          className="bg-zinc-100 justify-center text-stone-500 text-xs font-normal font-poppins pr-10"
+                        />
+                        <button
+                          type="button"
+                          className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500 hover:text-gray-700"
+                          onClick={() =>
+                            setShowConfirmPassword(!showConfirmPassword)
+                          }
+                        >
+                          {showConfirmPassword ? (
+                            <EyeOff size={16} />
+                          ) : (
+                            <Eye size={16} />
+                          )}
+                        </button>
+                      </div>
                       <Link
                         href="#"
-                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline font-poppins mt-1"
                       >
                         Forgot your password?
                       </Link>
@@ -95,7 +152,10 @@ export default function SignUp() {
                 </form>
               </CardContent>
               <CardFooter className="flex-col gap-5 ml-10 mr-10">
-                <Button type="submit" className="bg-gradient-to-br from-emerald-800 to-teal-500 hover:scale-105 duration-200 ease-in-out w-full">
+                <Button
+                  type="submit"
+                  className="bg-gradient-to-br from-emerald-800 to-teal-500 hover:scale-105 duration-200 ease-in-out w-full text-base font-semibold font-poppins h-[46px]"
+                >
                   SIGNUP
                 </Button>
                 <div className="flex gap-6">
@@ -109,9 +169,12 @@ export default function SignUp() {
                     <Image src={Apple} alt="Google" />
                   </div>
                 </div>
-                <p className="text-center font-semibold">
+                <p className="text-center font-semibold font-poppins">
                   Already have an account?{" "}
-                  <Link href="/signup" className="underline underline-offset-4 text-blue-600">
+                  <Link
+                    href="/signup"
+                    className="underline underline-offset-4 text-blue-600 font-poppins"
+                  >
                     Sign in
                   </Link>
                 </p>
@@ -121,7 +184,7 @@ export default function SignUp() {
           <div className="w-[50%]">
             <Image
               src={SignupImage}
-              className="w-[500px] h-[650px] object-cover"
+              className="w-[500px] h-[700px] object-cover"
               alt="Signup Image"
             />
           </div>
